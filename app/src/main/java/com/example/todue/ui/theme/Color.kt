a494297e6991c77eda0b618cd424ee3863c3b81c
amand <amanda.dyrby.andersen@gmail.com>
2023-11-06 11:23:54 +0100

    colors

--- app/src/main/java/com/example/todue/ui/theme/Color.kt
+++ app/src/main/java/com/example/todue/ui/theme/Color.kt
@@ -5,16 +5,9 @@ import androidx.compose.ui.graphics.Color
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
-
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
-
-
-
-
-
-
 
 //Light theme
 
@@ -27,8 +20,6 @@ val unselectedItemColor = Color(0xFF888888)
 val buttonColor = Color.LightGray
 
 
-
-
 //Dark theme (sucks atm)
 /*
 val textColor = Color(0xFFFFFFFF)
@@ -38,7 +29,38 @@ val barColor = Color(0xFF211F26)
 val selectedItemColor = Color(0xFF111967)
 val unselectedItemColor = Color(0xFF828EFF)
 val buttonColor = Color(0xFF828EFF)
-
-
-
  */
+
+
+//Figma colors
+
+val BoneWhite = Color(0xFEFDE7)
+val Turquoise = Color(0x2A9D8F)
+val DarkBlue = Color(0x264563)
+val Orange = Color(0xF4A261)
+val Red = Color(0xE76A51)
+val Yellow = Color(0xE9C46A)
+
+
+// Figma theme (not yet implemented)
+/*
+val textColorLight = BoneWhite
+val textColorDark = DarkBlue
+
+val itemColorToDo = Turquoise
+val itemColorTag = Orange
+val itemColorCalendar = Red
+val itemColorStats = Yellow
+
+val backgroundColor = BoneWhite
+val barColor = DarkBlue
+
+val selectedItemColorCalendar = Red
+val selectedItemColorTags = Orange
+val selectedItemColorOverview = Turquoise
+val selectedItemColorStats = Yellow
+val selectedItemColorSettings = DarkBlue
+
+val unselectedItemColor = BoneWhite
+val buttonColor = DarkBlue
+*/
