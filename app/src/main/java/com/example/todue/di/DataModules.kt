e23f712466c520f3c1fe24194839edde5c6c1d6a
Jossemand <s224564@student.dtu.dk>
2023-11-21 22:00:42 +0100

    Forgot to push the new files :/

---
+++ app/src/main/java/com/example/todue/di/DataModules.kt
@@ -0,0 +1,34 @@
+package com.example.todue.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.todue.dataLayer.source.local.TagDao
+import com.example.todue.dataLayer.source.local.ToDoDao
+import com.example.todue.dataLayer.source.local.ToDoDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModules {
+
+    @Singleton
+    @Provides
+    fun provideDataBase(@ApplicationContext context: Context): ToDoDatabase {
+        return Room.databaseBuilder(
+            context.applicationContext,
+            ToDoDatabase::class.java,
+            "todo.db"
+        ).build()
+    }
+
+    @Provides
+    fun provideToDoDao(database: ToDoDatabase) : ToDoDao = database.toDoDao
+
+    @Provides
+    fun provideTagDao(database: ToDoDatabase) : TagDao = database.tagDao
+}
