dc38fd685f04b2bfe2eeba9fd17b45073866e41e
Jossemand <s224564@student.dtu.dk>
2024-01-12 03:05:48 +0100

    Statistics is kinda okay looking. Needs some fixes tho

--- app/src/main/java/com/example/todue/ui/screens/statistics/Statistics.kt
+++ app/src/main/java/com/example/todue/ui/screens/statistics/Statistics.kt
@@ -1,36 +1,196 @@
 package com.example.todue.ui.screens.statistics
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.todue.state.ToDoState
 import com.example.todue.ui.event.ToDoEvent
 
+
+val defaultMaxHeight = 500.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun StatisticsScreen(
     toDoState: ToDoState,
     onToDoEvent: (ToDoEvent) -> Unit
 ) {
-    onToDoEvent(ToDoEvent.GetStatistics)
-    Column(
-        modifier = Modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Text(text = "Created ToDos: " + toDoState.totalAmountOfCreatedToDos)
-        Spacer(modifier = Modifier
-            .height(10.dp))
-        Text(text = "Finished ToDos: " + toDoState.totalAmountOfFinishedToDos)
-        Spacer(modifier = Modifier
-            .height(10.dp))
-        Text(text = "Unfinished ToDos: " + toDoState.totalAmountOfUnfinishedToDos)
+    val barChartItems = listOf(
+        toDoState.totalAmountOfCreatedToDos,
+        toDoState.totalAmountOfFinishedToDos,
+        toDoState.totalAmountOfUnfinishedToDos
+    )
+
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.background,
+                    titleContentColor = MaterialTheme.colorScheme.onBackground,
+                ),
+                title = {
+                    Text(
+                        "Statistics",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        fontSize = 30.sp
+                    )
+                },
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+
+            ) {
+            onToDoEvent(ToDoEvent.GetStatistics)
+            Row (
+                modifier = Modifier
+                .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.Bottom
+            ){
+
+            }
+            BarChart(values = barChartItems)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+
+                Column (
+                    modifier = Modifier,
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    Text(text = "" + toDoState.totalAmountOfCreatedToDos)
+                    Text(text = "Created")
+                    Text(text = "ToDos")
+                }
+                Column (
+                    modifier = Modifier,
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    Text(text = "" + toDoState.totalAmountOfUnfinishedToDos)
+                    Text(text = "Finished")
+                    Text(text = "ToDos")
+                }
+                Column (
+                    modifier = Modifier,
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    Text(text = "" + toDoState.totalAmountOfUnfinishedToDos)
+                    Text(text = "Unfinished")
+                    Text(text = "ToDos")
+                }
+            }
+        }
     }
 }
+
+@Composable
+internal fun BarChart(
+    modifier: Modifier = Modifier,
+    values: List<Int>,
+    maxHeight: Dp = defaultMaxHeight,
+
+    ) {
+
+    assert(values.isNotEmpty()) { "Input values are empty" }
+
+    val borderColor = MaterialTheme.colorScheme.primary
+    val density = LocalDensity.current
+    val strokeWidth = with(density) { 1.dp.toPx() }
+
+    Row(
+        modifier = modifier.then(
+            Modifier
+                .fillMaxWidth()
+                .height(maxHeight)
+                .drawBehind {
+                    // draw X-Axis
+                    drawLine(
+                        color = borderColor,
+                        start = Offset(0f, size.height),
+                        end = Offset(size.width, size.height),
+                        strokeWidth = strokeWidth
+                    )
+                    /*
+                    // draw Y-Axis
+                    drawLine(
+                        color = borderColor,
+                        start = Offset(0f, 0f),
+                        end = Offset(0f, size.height),
+                        strokeWidth = strokeWidth
+                    )
+
+                     */
+
+                }
+        ),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.Bottom
+    ) {
+        values.forEach { item ->
+            Bar(
+                value = item,
+                color = MaterialTheme.colorScheme.primary,
+                maxHeight = maxHeight,
+            )
+
+        }
+    }
+
+}
+
+@Composable
+private fun RowScope.Bar(
+    value: Int,
+    color: Color,
+    maxHeight: Dp,
+) {
+
+    val itemHeight = remember(value) { value * maxHeight.value / 100 }
+
+    Spacer(
+        modifier = Modifier
+            .padding(horizontal = 5.dp)
+            .height(itemHeight.dp)
+            .weight(1f)
+            .background(color)
+    )
+
+}
+
+
+
