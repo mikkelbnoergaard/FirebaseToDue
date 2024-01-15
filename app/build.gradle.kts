c715c2ca1ce16ac7b960d1e7269f2510a741f76b
Mikkel Nørgaard <mikkel.brunstedt@hotmail.dk>
2024-01-15 10:04:49 +0100

    removed firebase dependencies from gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,14 +55,6 @@ android {
 
 dependencies {
 
-    //firebase
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
-    implementation("com.google.android.gms:play-services-auth:20.7.0")
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("com.google.firebase:firebase-auth:22.3.0")
-    implementation("com.google.firebase:firebase-auth-ktx:22.3.0")
-
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
