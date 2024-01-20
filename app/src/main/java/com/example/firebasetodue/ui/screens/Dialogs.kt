f333b35a41e85d18c2abf22ebc22784c62022701
Mikkel Nørgaard <mikkel.brunstedt@hotmail.dk>
2024-01-20 09:53:07 +0100

    changed colors on text fields in CreateToDoDialog and EditToDoDialog

--- app/src/main/java/com/example/firebasetodue/ui/screens/Dialogs.kt
+++ app/src/main/java/com/example/firebasetodue/ui/screens/Dialogs.kt
@@ -35,7 +35,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
